8be2b7f0927968514c3c4f8f2ce69d7ae081f269
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 03:01:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 def square_array(array)
-  array.each! do |numbers|
-    numbers * numbers
-  end
+  array.each do |numbers|
+    squared_num = numbers * numbers
+    squared_num_array = []
+    squared_num_array.push(squared_num)
+    end
 end
