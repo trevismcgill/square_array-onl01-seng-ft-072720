a737184ab94a3586b29162cb28d960c46777cd58
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 03:10:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
+
 def square_array(array)
-  # your code here
+  squared_num_array = []
+  array.each do |numbers|
+    squared_num = numbers * numbers
+    squared_num_array.push(squared_num)
+  end
+  return squared_num_array
 end
